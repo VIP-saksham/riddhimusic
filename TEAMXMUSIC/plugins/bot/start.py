72acd326ef4d307764c8fdedcac78b1e3674c221
VIP-saksham <93027065+VIP-saksham@users.noreply.github.com>
2025-09-18 21:53:13 +0530

    Update start.py

--- TEAMXMUSIC/plugins/bot/start.py
+++ TEAMXMUSIC/plugins/bot/start.py
@@ -74,8 +74,8 @@ async def start_pm(client, message: Message, _):
             key = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton(text=_["S_B_8"], url=link),
-                        InlineKeyboardButton(text=_["S_B_9"], url=config.SUPPORT_CHAT),
+                        InlineKeyboardButton(text=_["S_B_3"], url=link),
+                        InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
                     ],
                 ]
             )
