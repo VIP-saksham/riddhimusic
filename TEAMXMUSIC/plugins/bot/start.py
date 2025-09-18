dd547bdc78bcf3f621f69a0b20609edd54f56844
VIP-saksham <93027065+VIP-saksham@users.noreply.github.com>
2025-09-18 21:51:15 +0530

    Update start.py

--- TEAMXMUSIC/plugins/bot/start.py
+++ TEAMXMUSIC/plugins/bot/start.py
@@ -1,15 +1,15 @@
 import time
-import random
+import random 
 from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from youtubesearchpython.__future__ import VideosSearch
 
 import config
-from AloneX import app
-from AloneX.misc import _boot_
-from AloneX.plugins.sudo.sudoers import sudoers_list
-from AloneX.utils.database import (
+from TEAMXMUSIC import app
+from TEAMXMUSIC.misc import SPECIAL_ID, _boot_
+from TEAMXMUSIC.plugins.sudo.sudoers import sudoers_list
+from TEAMXMUSIC.utils.database import (
     add_served_chat,
     add_served_user,
     blacklisted_chats,
@@ -17,46 +17,46 @@ from AloneX.utils.database import (
     is_banned_user,
     is_on_off,
 )
-from AloneX.utils.decorators.language import LanguageStart
-from AloneX.utils.formatters import get_readable_time
-from AloneX.utils.inline import help_pannel, private_panel, start_panel
+from TEAMXMUSIC.utils.decorators.language import LanguageStart
+from TEAMXMUSIC.utils.formatters import get_readable_time
+from TEAMXMUSIC.utils.inline import help_pannel, private_panel, start_panel
 from config import BANNED_USERS
 from strings import get_string
 
 
-# --- Optional Emoji List (for future use if needed) ---
 VALID_EMOJII = ["🔥", "💋", "🥺", "😒", "💖",
                 "💘", "💕", "✨", "🥰", "🍌", "💔",
                 "😓", "🫧"]
 
-
-# ---------------- PRIVATE START ----------------
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
 async def start_pm(client, message: Message, _):
     await add_served_user(message.from_user.id)
-
+  #  random_emoji = random.choice(VALID_EMOJII)  # Pick a valid emoji
+ #   try:
+      #  await message.react(random_emoji)
+   # except Exception as e:
+     #   print(f"Error reacting with emoji: {e}")
     if len(message.text.split()) > 1:
         name = message.text.split(None, 1)[1]
-
-        # --- Help panel ---
-        if name.startswith("help"):
+        if name[0:4] == "help":
             keyboard = help_pannel(_)
             return await message.reply_photo(
                 photo=config.START_IMG_URL,
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
             )
-
-        # --- Sudo list ---
-        if name.startswith("sud"):
+        if name[0:3] == "sud":
             await sudoers_list(client=client, message=message, _=_)
+            if await is_on_off(2):
+                return await app.send_message(
+                    chat_id=config.LOGGER_ID,
+                    text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>sᴜᴅᴏʟɪsᴛ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+                )
             return
-
-        # --- Track info ---
-        if name.startswith("inf"):
+        if name[0:3] == "inf":
             m = await message.reply_text("🔎")
-            query = name.replace("info_", "", 1)
+            query = (str(name)).replace("info_", "", 1)
             query = f"https://www.youtube.com/watch?v={query}"
             results = VideosSearch(query, limit=1)
             for result in (await results.next())["result"]:
@@ -68,7 +68,6 @@ async def start_pm(client, message: Message, _):
                 channel = result["channel"]["name"]
                 link = result["link"]
                 published = result["publishedTime"]
-
             searched_text = _["start_6"].format(
                 title, duration, views, published, channellink, channel, app.mention
             )
@@ -81,23 +80,31 @@ async def start_pm(client, message: Message, _):
                 ]
             )
             await m.delete()
-            return await app.send_photo(
+            await app.send_photo(
                 chat_id=message.chat.id,
                 photo=thumbnail,
                 caption=searched_text,
                 reply_markup=key,
             )
+            if await is_on_off(2):
+                return await app.send_message(
+                    chat_id=config.LOGGER_ID,
+                    text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+                )
+    else:
+        out = private_panel(_)
+        await message.reply_photo(
+            photo=config.START_IMG_URL,
+            caption=_["start_2"].format(message.from_user.mention, app.mention),
+            reply_markup=InlineKeyboardMarkup(out),
+        )
+        if await is_on_off(2):
+            return await app.send_message(
+                chat_id=config.LOGGER_ID,
+                text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+            )
 
-    # --- Normal start ---
-    out = private_panel(_)
-    await message.reply_photo(
-        photo=config.START_IMG_URL,
-        caption=_["start_2"].format(message.from_user.mention, app.mention),
-        reply_markup=InlineKeyboardMarkup(out),
-    )
 
-
-# ---------------- GROUP START ----------------
 @app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
 @LanguageStart
 async def start_gp(client, message: Message, _):
@@ -111,27 +118,21 @@ async def start_gp(client, message: Message, _):
     return await add_served_chat(message.chat.id)
 
 
-# ---------------- WELCOME ----------------
 @app.on_message(filters.new_chat_members, group=-1)
 async def welcome(client, message: Message):
     for member in message.new_chat_members:
         try:
             language = await get_lang(message.chat.id)
             _ = get_string(language)
-
-            # --- Auto ban banned users ---
             if await is_banned_user(member.id):
                 try:
                     await message.chat.ban_member(member.id)
                 except:
                     pass
-
-            # --- When bot joins group ---
             if member.id == app.id:
                 if message.chat.type != ChatType.SUPERGROUP:
                     await message.reply_text(_["start_4"])
                     return await app.leave_chat(message.chat.id)
-
                 if message.chat.id in await blacklisted_chats():
                     await message.reply_text(
                         _["start_5"].format(
