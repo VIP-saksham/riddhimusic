ed30024bb2bd4e795efcf880b0c017101d0909de
VIP-saksham <93027065+VIP-saksham@users.noreply.github.com>
2025-09-18 20:47:38 +0530

    Add files via upload

---
+++ TEAMXMUSIC/__main__.py
@@ -0,0 +1,78 @@
+import asyncio
+import importlib
+
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
+
+import config
+from TEAMXMUSIC import LOGGER, app, userbot
+from TEAMXMUSIC.core.call import JARVIS
+from TEAMXMUSIC.misc import sudo
+from TEAMXMUSIC.plugins import ALL_MODULES
+from TEAMXMUSIC.utils.database import get_banned_users, get_gbanned
+from TEAMXMUSIC.utils.cookie_handler import fetch_and_store_cookies 
+from config import BANNED_USERS
+
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER(__name__).error("ᴀssɪsᴛᴀɴᴛ sᴇssɪᴏɴ ɴᴏᴛ ғɪʟʟᴇᴅ, ᴘʟᴇᴀsᴇ ғɪʟʟ ᴀ ᴘʏʀᴏɢʀᴀᴍ sᴇssɪᴏɴ...")
+        exit()
+
+    # ✅ Try to fetch cookies at startup
+    try:
+        await fetch_and_store_cookies()
+        LOGGER("TEAMXMUSIC").info("ʏᴏᴜᴛᴜʙᴇ ᴄᴏᴏᴋɪᴇs ʟᴏᴀᴅᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ ✅")
+    except Exception as e:
+        LOGGER("TEAMXMUSIC").warning(f"⚠️ᴄᴏᴏᴋɪᴇ ᴇʀʀᴏʀ: {e}")
+
+
+    await sudo()
+
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("TEAMXMUSIC.plugins" + all_module)
+
+    LOGGER("TEAMXMUSIC.plugins").info("sᴜᴄᴄᴇssғᴜʟʟʏ ɪᴍᴘᴏʀᴛᴇᴅ ᴀʟʟ ᴍᴏᴅᴜʟᴇs...")
+
+    await userbot.start()
+    await JARVIS.start()
+
+    try:
+        await JARVIS.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+    except NoActiveGroupCall:
+        LOGGER("TEAMXMUSIC").error(
+            "ʙsᴅᴋ ᴠᴄ ᴛᴏ ᴏɴ ᴋᴀʀʟᴇ  ʟᴏɢ ɢʀᴏᴜᴘ\ᴄʜᴀɴɴᴇʟ ᴋɪ.\n\n ᴏɴ ᴋᴀʀᴋᴇ ᴀᴀ ᴛᴀʙ ᴛᴀᴋ ʙᴏᴛ ʙᴀɴᴅ ᴋᴀʀ ʀʜᴀ ʜᴏᴏɴ..."
+        )
+        exit()
+    except:
+        pass
+
+    await JARVIS.decorators()
+    LOGGER("TEAMXMUSIC").info(
+        "ᴍᴜsɪᴄ ʙᴏᴛ sᴛᴀʀᴛᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ, ɴᴏᴡ ɢɪʙ ʏᴏᴜʀ ɢɪʀʟғʀɪᴇɴᴅ ᴄʜᴜᴛ ɪɴ @ihellxyz"
+    )
+    await idle()
+    await app.stop()
+    await userbot.stop()
+    LOGGER("TEAMXMUSIC").info("ᴍᴀᴀ ᴄʜᴜᴅᴀ ᴍᴀɪɴ ʙᴏᴛ ʙᴀɴᴅ ᴋᴀʀ ʀʜᴀ Mᴜsɪᴄ Bᴏᴛ...")
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(init())
